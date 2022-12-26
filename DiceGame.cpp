8fa3bc23819b0ed225241fe28060e8073d2de2fb
Nico Kazassoglou <nico@kazassoglou.com>
2022-12-26 16:42:01 +0000

    add a game loop

--- DiceGame.cpp
+++ DiceGame.cpp
@@ -17,8 +17,8 @@ int main()
     int roundsOfPlay = 5; // fixed number of rounds to play
     int target = 20;
 
-    int player1Score;
-    int player2Score;
+    int player1Score = 0;
+    int player2Score = 0;
 
     std::string in;
 
@@ -29,20 +29,26 @@ int main()
     //std::cout << "Player 2 enter your name: \n";
     //std::cin >> namePlayer2;
 
-    std::cout << "Player 1 press r to roll: \n";
-    std::cin >> in;
-    if (in == "r")
-    {
-        player1Score = GetRandomNum();
-    }
-    std::cout << "Player 2 press r to roll: \n";
-    std::cin >> in;
-    if (in == "r")
-    {
-        player2Score = GetRandomNum();
-    }
+    for(int i=0; i< roundsOfPlay; i++)
+    { 
+        std::cout << "Player 1 press r to roll: \n";
+        std::cin >> in;
+        if (in == "r")
+        {
+            player1Score += GetRandomNum(); // is the same as: player1Score = player1Score + GetRandomNum();
+        }
 
+        std::cout << "Player 2 press r to roll: \n";
+        std::cin >> in;
+        if (in == "r")
+        {
+            player2Score += GetRandomNum();
+        }
 
+        // print result after each round
+        std::cout << "Player 1 Score: " << player1Score << "\n";
+        std::cout << "Player 2 Score: " << player2Score << "\n";
+    }
     int b = 7;
 
 }
