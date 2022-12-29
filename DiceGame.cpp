2815cc9c7b02063ccc143809625b758e14f53e5e
Nico Kazassoglou <nico@kazassoglou.com>
2022-12-29 15:42:56 +0000

    add some new lines

--- DiceGame.cpp
+++ DiceGame.cpp
@@ -42,8 +42,10 @@ int main()
     // Enter names
     std::cout << "Player 1 enter your name: \n";
     std::cin >> namePlayer1;
+    std::cout << "\n";
     std::cout << "Player 2 enter your name: \n";
     std::cin >> namePlayer2;
+    std::cout << "\n";
 
     // Main Game loop
     for(int i=0; i< roundsOfPlay; i++)
@@ -53,14 +55,14 @@ int main()
         if (numDice == 1)
         {
             dice1 = GetRandomNum();
-            std::cout << namePlayer1 << " rolled: " << dice1 << "\n";
+            std::cout << namePlayer1 << " rolled: " << dice1 << "\n" << "\n";
             player1Score += dice1;
         }
         if (numDice == 2)
         {
             dice1 = GetRandomNum();
             dice2 = GetRandomNum();
-            std::cout << namePlayer1 << " rolled: " << dice1 << ", " << dice2 << "\n";
+            std::cout << namePlayer1 << " rolled: " << dice1 << ", " << dice2 << "\n" << "\n";
             player1Score += dice1 + dice2;
         }
         if (numDice == 3)
@@ -68,7 +70,7 @@ int main()
             dice1 = GetRandomNum();
             dice2 = GetRandomNum();
             dice3 = GetRandomNum();
-            std::cout << namePlayer1 << " rolled: " << dice1 << ", " << dice2 << ", " << dice3 << "\n";
+            std::cout << namePlayer1 << " rolled: " << dice1 << ", " << dice2 << ", " << dice3 << "\n" << "\n";
             player1Score += dice1 + dice2 + dice3;
         }
 
@@ -77,14 +79,14 @@ int main()
         if (numDice == 1)
         {
             dice1 = GetRandomNum();
-            std::cout << namePlayer2 << " rolled: " << dice1 << "\n";
+            std::cout << namePlayer2 << " rolled: " << dice1 << "\n" << "\n";
             player2Score += dice1;
         }
         if (numDice == 2)
         {
             dice1 = GetRandomNum();
             dice2 = GetRandomNum();
-            std::cout << namePlayer2 << " rolled: " << dice1 << ", " << dice2 << "\n";
+            std::cout << namePlayer2 << " rolled: " << dice1 << ", " << dice2 << "\n" << "\n";
             player2Score += dice1 + dice2;
         }
         if (numDice == 3)
@@ -92,13 +94,15 @@ int main()
             dice1 = GetRandomNum();
             dice2 = GetRandomNum();
             dice3 = GetRandomNum();
-            std::cout << namePlayer2 << " rolled: " << dice1 << ", " << dice2 << ", " << dice3 << "\n";
+            std::cout << namePlayer2 << " rolled: " << dice1 << ", " << dice2 << ", " << dice3 << "\n" << "\n";
             player2Score += dice1 + dice2 + dice3;
         }
 
         // print result after each round
         std::cout << "Player 1 Score: " << player1Score << "\n";
+        std::cout << "\n";
         std::cout << "Player 2 Score: " << player2Score << "\n";
+        std::cout << "\n";
     }
 
     // finish the game and print the winner:
@@ -119,11 +123,22 @@ int main()
 
 }
 
-int GetRandomNum()
+int GetRandomNum() // Generate a random number between 1 and 6
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(750));
-    srand(time(0)); // set time for random number generator
-    //std::this_thread::sleep_for(std::chrono::seconds(2));
-    return (rand() % 6 + 1);
+    // use namespace chrono
+    using namespace std::chrono;
+    // and include literals to be able to use 0.5s for seconds
+    using namespace std::literals::chrono_literals;
+    // get the current time in nanoseconds
+    nanoseconds ns = duration_cast<nanoseconds>(system_clock::now().time_since_epoch());
+    // store the count in an int to be used as the seed for the random num gen
+    int seed = ns.count();
+    std::this_thread::sleep_for(1ms); // sleep the thread in order to get a new seed every time
+
+    std::srand(seed); // use the seed in ms to get better random numbers
+
+    int randomNumber = (std::rand() % 6 + 1); // clamp the number to only allow values between 1 and 6
+    return randomNumber;
+    // return std::rand(); // INFO: this also works and does not require a temporary int.
 }
 
