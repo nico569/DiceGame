1b1ffe449e17e494e562d3fedace38d730e56f5d
Nico Kazassoglou <nico@kazassoglou.com>
2023-01-03 21:25:44 +0000

    tidy up the game code

--- DiceGame.cpp
+++ DiceGame.cpp
@@ -5,8 +5,6 @@
 #include "DiceGame.h"
 #include "Player.h"
 
-
-
 int main()
 {
    
@@ -27,7 +25,8 @@ int main()
     std::cin >> roundsOfPlay;
 
     // work out the target based on the number of rounds 
-    target = roundsOfPlay * 3 * GetRandomNum();
+    RandomNumberGenerator randomNumber;
+    target = roundsOfPlay * 3 * randomNumber.GetRandomNum();
     std::cout << "The target you are trying to get closest to is: " << target << "\n";
     std::cout << "\n";
 
@@ -44,18 +43,17 @@ int main()
     { 
         std::cout << player1.getName() <<" press 1,2 or 3 to roll the number of dice you want: \n";
         std::cin >> numDice;
-        rollDice(dice, numDice);
-        updatePlayerDiceRolled(dice, numDice, player1.getName());
-        player1.calcPlayerScore(dice, numDice, target);
-        updatePlayerScore(player1.getName(), player1.getScore());
+        player1.rollDice(numDice);
+        player1.LogDiceRolled(numDice);
+        player1.calcPlayerScore(numDice, target);
+        player1.LogScore();
 
         std::cout << player2.getName() << " press 1,2 or 3 to roll the number of dice you want: \n";
         std::cin >> numDice;
-        rollDice(dice, numDice);
-        updatePlayerDiceRolled(dice, numDice, player2.getName());
-        player2.calcPlayerScore(dice, numDice, target);
-        updatePlayerScore(player2.getName(), player2.getScore());
-
+        player2.rollDice(numDice);
+        player2.LogDiceRolled(numDice);
+        player2.calcPlayerScore(numDice, target);
+        player2.LogScore();
 
     }
 
@@ -75,59 +73,6 @@ int main()
     }
     
 
-}
-
-int GetRandomNum() // Generate a random number between 1 and 6
-{
-    // use namespace chrono
-    using namespace std::chrono;
-    // and include literals to be able to use 0.5s for seconds
-    using namespace std::literals::chrono_literals;
-    // get the current time in nanoseconds
-    nanoseconds ns = duration_cast<nanoseconds>(system_clock::now().time_since_epoch());
-    // store the count in an int to be used as the seed for the random num gen
-    int seed = ns.count();
-    std::this_thread::sleep_for(1ms); // sleep the thread in order to get a new seed every time
-
-    std::srand(seed); // use the seed in ms to get better random numbers
-
-    int randomNumber = (std::rand() % 6 + 1); // clamp the number to only allow values between 1 and 6
-    return randomNumber;
-    // return std::rand(); // INFO: this also works and does not require a temporary int.
-}
-
-void rollDice(int dice[3], int numberOfDice)
-{
-    for (int i = 0; i < numberOfDice; i++)
-    {
-        dice[i] = GetRandomNum();
-    }
-}
-
-void updatePlayerDiceRolled(int dice[3], int numberOfDice, std::string PlayerName)
-{
-    std::cout << PlayerName << " you've rolled: ";
-    
-    for (int i = 0; i < numberOfDice; i++)
-    {
-        std::cout << dice[i];
-        if (i < (numberOfDice - 1) )
-        {
-            std::cout << ", ";
-        }
-        else
-        {
-            std::cout << "." << "\n";
-        }
-    }
-    
-    std::cout << "\n";
-}
-
-void updatePlayerScore(std::string PlayerName, int score)
-{
-    std::cout << PlayerName << " your current score is: " << score << "\n";
-    std::cout << "\n";
 }
 
 
--- DiceGame.h
+++ DiceGame.h
@@ -2,19 +2,7 @@
 
 #include <iostream>
 #include <cstdlib>
-#include <ctime>
-#include <chrono>
-#include <thread>
 
-int GetRandomNum();
-
-void rollDice(int dice[3], int numberOfDice);
-
-void updatePlayerDiceRolled(int dice[3], int numberOfDice, std::string PlayerName);
-
-void updatePlayerScore(std::string PlayerName, int score);
-
-int dice[3] = { 0 };
 
 int readInt(std::istream& stream);
 
