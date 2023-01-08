8a8a0a8de5124f81d9a01c79fbeb25c602daba14
Nico Kazassoglou <nico@kazassoglou.com>
2023-01-08 13:23:11 +0000

    implement the calss in the main program

--- DiceGame.cpp
+++ DiceGame.cpp
@@ -3,45 +3,49 @@
 //
 
 #include "DiceGame.h"
+#include "DiceGameCore.h"
 #include "Player.h"
 
 int main()
 {
+    DiceGameCore diceGameCore;
    
     Player player1;
     Player player2;
 
-    int roundsOfPlay = 1; // fixed number of rounds to play
-    int target = 30;
-    
     // Game Start:
 
     // Greeting:
     std::cout << "Hi and welcome to this awesome game!\n";
     std::cout << "\n";
-    std::cout << "How many rounds do you want to play?\n";
-    std::cin >> roundsOfPlay;
 
-    // work out the target based on the number of rounds 
-    RandomNumberGenerator randomNumber;
-    target = roundsOfPlay * 3 * randomNumber.GetRandomNum();
+    std::cout << "How many players are playing?\n";
+    diceGameCore.setNumberOfPLayers(readInt(std::cin));
+
+    std::cout << "How many rounds do you want to play?\n";
+    diceGameCore.setRoundsOfPlay(readInt(std::cin));
+
+    diceGameCore.calcTarget();
+
     // upodate each player with the tageret score:
-    player1.setTargetScore(target);
-    player2.setTargetScore(target);
+    player1.setTargetScore(diceGameCore.getTarget());
+    player2.setTargetScore(diceGameCore.getTarget());
+
     // tell the players the target score
-    std::cout << "The target you are trying to get closest to is: " << target << "\n";
+    std::cout << "The target you are trying to get closest to is: " << diceGameCore.getTarget() << "\n";
     std::cout << "\n";
 
     // Enter names
     std::cout << "Player 1 enter your name: \n";
     player1.setName(readString(std::cin));
     std::cout << "\n";
+
     std::cout << "Player 2 enter your name: \n";
     player2.setName(readString(std::cin));
     std::cout << "\n";
 
     // Main Game loop
-    for(int i=0; i< roundsOfPlay; i++)
+    for(int i=0; i< diceGameCore.getRoundsOfPlay(); i++)
     { 
         player1.LogQueryNumDice();
         player1.goPlay(readInt(std::cin));
@@ -52,11 +56,11 @@ int main()
 
     // finish the game and calc and print the winner:
 
-    if (target - player1.getScore() == target - player2.getScore())
+    if (diceGameCore.getTarget() - player1.getScore() == diceGameCore.getTarget() - player2.getScore())
     {
         std::cout << "Its a Draw! \n";
     }
-    else if (target - player1.getScore() < target - player2.getScore())
+    else if (diceGameCore.getTarget() - player1.getScore() < diceGameCore.getTarget() - player2.getScore())
     {
         std::cout << "The Winner is: " << player1.getName() << "\n";
     }
